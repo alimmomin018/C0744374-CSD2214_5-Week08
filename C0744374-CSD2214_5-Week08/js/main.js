b0f8b228c9e31483ae9d4788de9d7fcaf3fed1b4
Alim Momin <alimmomin018@gmail.com>
2019-02-28 11:12:50 -0500

    commit 11:12

--- C0744374-CSD2214_5-Week08/js/main.js
+++ C0744374-CSD2214_5-Week08/js/main.js
@@ -1,1 +1,28 @@
-﻿// Your code here!
+﻿
+//The circle below is called an object literal
+const circle = {
+    radius: 1,
+    location:{
+        x: 1,
+        y:1
+    },
+    draw() {
+        console.log('draw' + this.location.x);
+    }
+};
+
+circle.draw();
+
+//Factory function
+function createCircle(radius){
+    return {
+        radius,  //radius is same as radius: radius
+        draw() {
+            console.log('drawaa');
+        }
+    };
+}
+
+const creacteCricle = createCircle(1);
+console.log(creacteCricle);
+creacteCricle.draw();
