49bfc2f18c6602d73b50b45e4db4e3390e929a68
Alim Momin <alimmomin018@gmail.com>
2019-02-28 12:20:45 -0500

    commit

--- C0744374-CSD2214_5-Week08/js/main.js
+++ C0744374-CSD2214_5-Week08/js/main.js
@@ -10,10 +10,10 @@ const circle = {
         console.log('draw' + this.location.x);
     }
 };
-
 circle.draw();
 
-//Factory function
+
+//Factory function 
 function createCircle(radius){
     return {
         radius,  //radius is same as radius: radius
@@ -22,7 +22,35 @@ function createCircle(radius){
         }
     };
 }
-
 const creacteCricle = createCircle(1);
 creacteCricle.draw();
 
+
+//Constructor Function  //this fucntion doesnt have a return type and when you want to create an object you have to use new
+function Circle(radius) {
+    console.log(this);
+    this.radius = radius;
+    this.draw = function () {
+        console.log('DRAW');
+    };
+}
+const another = new Circle(1);
+
+//challenge number one
+function forLoop(start,until,stepDown) { //forLoop has 2 parameter start and until for the for loop
+    sum = 0; //intializing sum to 0
+    for (i = start; i >= until; i -= stepDown) {
+        sum += i;
+    }
+    document.getElementById('forLoopOutput').innerHTML = sum; // output sum to html page by get element by id
+}
+forLoop(1109, 119,11); //calling the function
+
+//challenge number two
+function onClick() {
+    start = document.getElementById('start').innerHTML;
+    end = document.getElementById('end').innerHTML;
+    stepDown = document.getElementById('stepDown').innerHTML;
+    forLoop(start, end, stepDown);
+    start = document.getElementById('forLoopOutputFromUser').innerHTML;
+}
